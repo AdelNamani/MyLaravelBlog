5c65eea2712d5df935eb2b169de8cad530cc76e1
AdelNamani <ga_namani@esi.dz>
2018-10-17 18:02:43 +0100

    New version

---
+++ resources/views/Post/edit.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ga_na
+ * Date: 8/19/2018
+ * Time: 11:18 AM
+ */
---
+++ resources/views/Post/show.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ga_na
+ * Date: 8/17/2018
+ * Time: 12:38 AM
+ */
